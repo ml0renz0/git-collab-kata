0fcc8140076ae6bea1945813fa89e162013a20cf
Marcos Lorenzo <11718206+ml0renz0@users.noreply.github.com>
2026-05-27 19:22:52 +0200

    feature: cast add inputs to int

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,6 +6,10 @@ def test_add():
     assert add(2, 3) == 5
 
 
+def test_add_casts_string_numbers_to_int():
+    assert add("2", "3") == 5
+
+
 def test_subtract():
     assert subtract(5, 3) == 2
 
