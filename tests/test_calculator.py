7385009f28d00f2469ca3a2cc3fae78f731e101a
Marcos Lorenzo <11718206+ml0renz0@users.noreply.github.com>
2026-06-09 18:25:52 +0200

    feature: multiply

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from app.calculator import add, subtract
+from app.calculator import add, subtract, multiply
 
 
 def test_add():
@@ -8,3 +8,5 @@ def test_add():
 def test_subtract():
     assert subtract(5, 3) == 2
 
+def test_multiply():
+    assert multiply(5, 3) == 15
