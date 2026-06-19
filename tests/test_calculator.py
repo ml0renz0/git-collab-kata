e2ee8bd941eb9e1eb4902b006ca34bb95d28f2a9
Marcos Lorenzo <11718206+ml0renz0@users.noreply.github.com>
2026-06-19 19:02:53 +0200

    feature: add cast int
    Signed-off-by: Marcos Lorenzo <11718206+ml0renz0@users.noreply.github.com>

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,6 +5,9 @@ from app.calculator import add, subtract, divide, modulus, multiply, exponentiat
 def test_add():
     assert add(2, 3) == 5
 
+def test_add_casts_string_numbers_to_int():
+    assert add("2", "3") == 5
+
 def test_subtract():
     assert subtract(5, 3) == 2
 
