dd44b2a50c71bb2b7fda7975b28ff627bbc90724
Marcos Lorenzo de Santiago <11718206+ml0renz0@users.noreply.github.com>
2026-06-19 18:27:32 +0200

    feature: add divide operation
    Prerequisito del ejercicio 03.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.calculator import add, factorial, subtract
+from app.calculator import add, divide, factorial, subtract
 
 
 def test_add():
@@ -9,6 +9,10 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_divide():
+    assert divide(10, 2) == 5
 
 
 def test_factorial_zero():
